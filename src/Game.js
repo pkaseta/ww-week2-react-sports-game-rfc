d71b171bdccb3885589e2408bdd80eb3edea9fc5
Paul Kaseta Jr <pkaseta@Pauls-Mac-mini.local>
2021-03-04 18:56:19 -0500

    added additional starter code

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,23 @@
-const NHLteams = [
-    { city: 'Detroit', teamName: 'Red Wings', logoUrl: './img/wingsLogo.png', abbre: 'Det', arena: 'Little Caesars Arena' },
-    { city: 'Colorado', teamName: 'Avalanche', logoUrl: './img/avsLogo.png', abbre: 'Col', arena: 'Ball Arena' },
-    { city: 'Philladelphia', teamName: 'Flyers', logoUrl: './img/flyersLogo.png', abbre: 'Phi', arena: 'Wells Fargo Center' },
-    { city: 'Toronto', teamName: 'Maple Leafs', logoUrl: './img/leafsLogo.png', abbre: 'Tor', arena: 'Scotiabank Arena' }
-]
-// Game Functional component
+import React, { useState, useEffect } from 'react';
+
+import wingsLogo from './img/wingsLogo.png'
+import avsLogo from './img/avsLogo.png'
+import flyersLogo from './img/flyersLogo.png'
+import leafsLogo from './img/leafsLogo.png'
+
+
+const Game = () => {
+    const NHLteams = [
+        { city: 'Detroit', teamName: 'Red Wings', logoUrl: wingsLogo, abbre: 'Det', arena: 'Little Caesars Arena' },
+        { city: 'Colorado', teamName: 'Avalanche', logoUrl: avsLogo, abbre: 'Col', arena: 'Ball Arena' },
+        { city: 'Philladelphia', teamName: 'Flyers', logoUrl: flyersLogo, abbre: 'Phi', arena: 'Wells Fargo Center' },
+        { city: 'Toronto', teamName: 'Maple Leafs', logoUrl: leafsLogo, abbre: 'Tor', arena: 'Scotiabank Arena' }
+    ]
+
+    return (
+       
+    );
+}
+
+export default Game;
+
--- src/Teams.js
+++ src/Teams.js
@@ -1,1 +1,6 @@
-// Team functional component
+
+import uifx from 'uifx'
+import shotAudio from './sounds/hockeystick.wav'
+import goalAudio from './sounds/Vanc2.wav'
+
+
